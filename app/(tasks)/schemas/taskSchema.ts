b1700c4cec52717df05fa2bf44a49c093229fcf2
Hayk <hayk.sargsyan9292@gmail.com>
2025-10-29 01:27:51 +0400

    added task schema for validations and types also keep mock of tasks in memory as storage imitation

---
+++ app/(tasks)/schemas/taskSchema.ts
@@ -0,0 +1,15 @@
+import zod from "zod";
+
+export const TaskSchema = zod.object({
+  id: zod.string().optional(),
+  title: zod
+    .string()
+    .trim()
+    .min(3, { message: "Must be at least 3 characters" }),
+  // description: zod.string().max(200, "Description too long").optional(),
+  // priority: zod.enum(["low", "medium", "high"]),
+  // dueDate: zod.string().optional(),
+  // completed: zod.boolean().default(false),
+});
+
+export type Task = zod.infer<typeof TaskSchema>;
